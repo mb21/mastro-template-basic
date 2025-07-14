ebb350853475d4a2f90b2db78089a816a6ba070a
mb21 <mb21@users.noreply.github.com>
2025-07-14 13:22:08 +0200

    test cdn cache

--- server.ts
+++ server.ts
@@ -1,3 +1,9 @@
 import server from "mastro/server";
 
-Deno.serve(server.fetch)
+Deno.serve(async req => {
+  const res = await server.fetch(req)
+  if (Deno.env.get("DENO_DEPLOYMENT_ID")) {
+    res.headers.set("Cache-Control", "s-maxage=604800");
+  }
+  return res
+})
